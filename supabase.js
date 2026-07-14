f6b18eade8ebe08b7edaa4569a75bd1bbc676b33
rayn76678-dev <rayn76678@gmail.com>
2026-07-14 23:02:43 +0800

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,13 @@
+const SUPABASE_URL =
+"https://jhwxslcnbaqxqdlijutg.supabase.co";
+
+
+const SUPABASE_KEY =
+"sb_publishable_你的那串";
+
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
